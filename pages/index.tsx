154637e0de7bf9e9149a0ca866eeace7e2560f0c
Simon Opheim <simon.opheim@gmail.com>
2021-06-13 09:01:26 +0200

    feat: add to homescreen prompt in chrome and edge

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,44 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [deferredPrompt, setDeferredPrompt] =
+    useState<BeforeInstallPromptEvent>(null);
+  const [showInstall, setShowInstall] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener(
+      'beforeinstallprompt',
+      (e: BeforeInstallPromptEvent) => {
+        // Prevent Chrome 67 and earlier from automatically showing the prompt
+        e.preventDefault();
+
+        // Stash the event so it can be triggered later.
+        setDeferredPrompt(e);
+        // Update UI to notify the user they can add to home screen
+        setShowInstall(true);
+      }
+    );
+  }, []);
+
+  const handleClickInstall = () => {
+    setShowInstall(false);
+
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('Brukeren aksepterte A2HS prompt');
+        } else {
+          console.log('Brukeren avslo A2HS prompt');
+        }
+        setDeferredPrompt(null);
+      });
+    }
+  };
+
   return (
     <div className="h-screen grid grid-rows-2">
       <header className="flex flex-col justify-evenly items-center">
@@ -13,17 +50,25 @@ export default function Home() {
           alt="Ladekalkulator logo. Ladestasjon med kalkulator."
         />
       </header>
-      <main className="flex flex-col items-center space-y-3 justify-center mx-auto w-56">
+      <main className="flex flex-col items-center space-y-5 justify-center mx-auto w-56">
         <Link href="/billigst">
-          <a className="text-center py-4 bg-blue-300 rounded mb-2 w-full">
+          <a className="text-center py-4 bg-blue-300 rounded w-full">
             Hvor er det billigst å lade?
           </a>
         </Link>
         <Link href="/tid">
-          <a className="text-center py-4 bg-blue-300 rounded mt-2 w-full">
+          <a className="text-center py-4 bg-blue-300 rounded w-full">
             Hvor lenge må jeg lade?
           </a>
         </Link>
+        <button
+          onClick={handleClickInstall}
+          className={`bg-green-300 rounded text-center py-4 w-full ${
+            showInstall ? 'block' : 'hidden'
+          }`}
+        >
+          Legg til hjem-skjerm
+        </button>
       </main>
     </div>
   );
