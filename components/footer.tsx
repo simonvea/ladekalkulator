7c27142ed33573a8f8b7c8b3d1666bd5e543fb3b
Simon Opheim <simon.opheim@gmail.com>
2021-06-24 18:34:56 +0200

    feat: add navigation footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,29 +1,43 @@
 import { FC } from 'react';
 import Link from 'next/link';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOnOutlined';
+import InfoIcon from '@material-ui/icons/InfoOutlined';
 
-type FooterProps = {
-  showBackToHome?: boolean;
-};
-
-const Footer: FC<FooterProps> = ({ showBackToHome }) => {
+const Footer: FC = () => {
   return (
-    <footer className="flex flex-col justify-end items-center space-y-10 w-full">
-      {showBackToHome && (
-        <Link href="/">
-          <a className="p-3 bg-gray-300 rounded">Tilbake til fremsiden</a>
-        </Link>
-      )}
-      <section className="mb-2">
-        Laget av{' '}
-        <a
-          href="https://www.simonsier.no/about"
-          rel="noreferrer noopener"
-          className="underline"
-        >
-          Simon Opheim
-        </a>
-      </section>
-    </footer>
+    <div className="h-16 w-full">
+      <footer className="flex h-16 shadow bg-white items-center w-full fixed bottom-0">
+        <nav className="w-full h-full">
+          <ul className="flex w-full h-full justify-evenly items-center">
+            <li>
+              <Link href="/tid">
+                <a className="text-center">
+                  <AccessTimeIcon fontSize="large" />
+                  <div>Tid</div>
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/billigst">
+                <a className="text-center">
+                  <MonetizationOnIcon fontSize="large" />
+                  <div>Pris</div>
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a className="text-center">
+                  <InfoIcon fontSize="large" />
+                  <div>Info</div>
+                </a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </footer>
+    </div>
   );
 };
 
