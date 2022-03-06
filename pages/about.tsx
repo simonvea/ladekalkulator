6e0790fccd6db8b012c3395fcd925b352518fab4
Simon Opheim <simon.opheim@fjellsport.no>
2022-03-06 18:59:01 +0100

    feat: add date for when prices was last updated

--- pages/about.tsx
+++ pages/about.tsx
@@ -26,6 +26,10 @@ const About: FC = () => {
           </a>
           .
         </article>
+        <aside className="mt-4">
+          Ladepriser sist oppdatert{' '}
+          <time dateTime="2022-03-06">06.03.2022</time>.
+        </aside>
       </main>
 
       <Footer />
